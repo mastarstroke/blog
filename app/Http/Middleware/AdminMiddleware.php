ec432721428c0532d4da327de8a707424b548219
James Adeyemo <96504630+mastarstroke@users.noreply.github.com>
2024-06-19 16:38:40 +0100

    Update AdminMiddleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -22,7 +22,7 @@ class AdminMiddleware
             return $next($request); 
         }
         else{
-            return redirect()->route('login/register');
+            return redirect()->route('login');
         }
     }
 }
